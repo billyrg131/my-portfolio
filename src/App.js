12e1111f4c07d09c97107a97f0b6959e4d3496d9
billyrg131 <85501810+billyrg131@users.noreply.github.com>
2024-04-17 10:23:06 +0300

    c5

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
 				<div className="nav">
 					<p id="name-hd" style={{ fontSize: '28px' }}>
 						<a href="https://billyrg131.github.io/my-portfolio/">
-							Bill Rogers
+							Bill Rogers Mutende
 						</a>
 					</p>
 					<p style={{ fontSize: '26px' }}>
